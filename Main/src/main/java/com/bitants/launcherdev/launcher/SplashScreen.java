2b6248f8fc00bce9a1fef8eaab454966fb7e4131
xbee <xbee@outlook.com>
2015-06-05 23:52:25 +0800

    add splashscreen

--- Main/src/main/java/com/bitants/launcherdev/launcher/SplashScreen.java
+++ Main/src/main/java/com/bitants/launcherdev/launcher/SplashScreen.java
@@ -1,7 +1,42 @@
 package com.bitants.launcherdev.launcher;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.bitants.launcher.R;
+
 /**
  * Created by michael on 15/6/5.
  */
-public class SplashScreen {
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent("com.bitants.launcherdev.launcher.MAINACTIVITY");
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
 }
