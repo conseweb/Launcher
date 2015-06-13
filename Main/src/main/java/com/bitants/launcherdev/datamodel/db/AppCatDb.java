f6361816b399bc164c63ce4e8e05da7f1fe97214
xbee <xbee@outlook.com>
2015-06-14 01:18:13 +0800

    new appcat db

--- Main/src/main/java/com/bitants/launcherdev/datamodel/db/AppCatDb.java
+++ Main/src/main/java/com/bitants/launcherdev/datamodel/db/AppCatDb.java
@@ -46,29 +46,21 @@ public class AppCatDb extends SQLiteAssetHelper {
         };
 
         try {
-            Cursor cursor = db.query(
-                    AppCatEntry.TABLE_NAME,  // The table to query
-                    projection,                               // The columns to return
-                    AppCatEntry.COLUMN_NAME_PKG,              // The columns for the WHERE clause
-                    selectionArgs,                            // The values for the WHERE clause
-                    null,                                     // don't group the rows
-                    null,                                     // don't filter by row groups
-                    null                                      // The sort order
-            );
+            Cursor cursor = db.rawQuery("select cat from appcats where pkg = '" + pkg.trim() + "'",
+                    null);
 
             cursor.moveToFirst();
             ret = cursor.getInt(
                     cursor.getColumnIndex(AppCatEntry.COLUMN_NAME_CAT)
             );
 
-            // maybe -1 : means does not exists in db ; 1-24
-            return ret;
-
         } catch (Exception e) {
             Log.e(TAG, e.toString());
-            return -1;
+            ret = -1;
         }
 
+        // maybe -1 : means does not exists in db ; 1-24
+        return ret;
     }
 
     public final String queryCatNameByPkg(String pkg, Context ctx) {
@@ -77,59 +69,78 @@ public class AppCatDb extends SQLiteAssetHelper {
 
         // max folder num in one screen: 16 - 20
         switch (cat) {
+            case 100:
             case -1:
                 result = ctx.getString(R.string.cat_other);
                 break;
+            case 101:
             case 1: // Games
             case 7: // Entertainment
                 result = ctx.getString(R.string.cat_game);
                 break;
+            case 102:
             case 2: // Books & Reference
             case 4: // Comics
             case 14: // News & Magazines
                 result = ctx.getString(R.string.cat_newsreading);
                 break;
+            case 103:
             case 3: // Business
                 result = ctx.getString(R.string.cat_bussiness);
                 break;
+            case 104: // cloud storage
+                result = ctx.getString(R.string.cat_cloud);
+                break;
+            case 105:
             case 5:  // Communication
             case 19: // Social
                 result = ctx.getString(R.string.cat_social);
                 break;
+            case 106:
             case 6: // Education
                 result = ctx.getString(R.string.cat_education);
                 break;
+            case 107:
             case 8: // Finance
                 result = ctx.getString(R.string.cat_finance);
                 break;
+            case 108:
             case 9: // Health & Fitness
             case 12: // Medical
                 result = ctx.getString(R.string.cat_health);
                 break;
+            case 109:
             case 10: // Lifestyle
                 result = ctx.getString(R.string.cat_life);
                 break;
+            case 110:
             case 11: // Media & Video
             case 13: // Music & Audio
                 result = ctx.getString(R.string.cat_av);
                 break;
+            case 111:
             case 15: // Personalization
                 result = ctx.getString(R.string.cat_personalization);
                 break;
+            case 112:
             case 16: // Photography
                 result = ctx.getString(R.string.cat_photography);
                 break;
+            case 113:
             case 17: // Productivity
             case 21: // Tools
             case 24: // Weather
                 result = ctx.getString(R.string.cat_tools);
                 break;
+            case 114:
             case 18: // Shopping
                 result = ctx.getString(R.string.cat_shopping);
                 break;
+            case 115:
             case 20: // Sports
                 result = ctx.getString(R.string.cat_sports);
                 break;
+            case 116:
             case 22: // Transportation
             case 23: // Travel & Local
                 result = ctx.getString(R.string.cat_travelnlocal);
